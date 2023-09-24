4c2f1d06a43ed087e6717f860f433971803d36f4
Mrudul <mrudulkolambe02@gmail.com>
2023-09-24 20:55:01 +0530

    Initial Commit

--- routes/User.js
+++ routes/User.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { signInUser, createUser, userProfile } = require("../controllers/User");
+const { signInUser, createUser, userProfile, updateProfile } = require("../controllers/User");
 const VerifyUserToken = require("../middlewares/User");
 const router = express.Router();
 
@@ -9,6 +9,6 @@ router.post("/signin", signInUser);
 
 router.get("/profile", VerifyUserToken, userProfile);
 
-router.patch("/update", VerifyUserToken, userProfile);
+router.patch("/update", VerifyUserToken, updateProfile);
 
 module.exports = router
